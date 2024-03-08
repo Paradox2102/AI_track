2812a8d3d7d3053b6bd2336195be9a7eb1e6da68
Norman209 <147957226+Norman209@users.noreply.github.com>
2024-03-07 22:09:48 -0800

    Update server_for_tracking.py

--- server_for_tracking.py
+++ server_for_tracking.py
@@ -1,149 +1,128 @@
 import socket
 import time
-import sys
+import threading
+import queue
 import cv2
 import imutils
 from yoloDet import YoloTRT
-import os
-import threading
-import pycuda.driver as cuda  # Add this line to initialize CUDA context
-#MAKE SURE TO IMPLEMENT THIS: nano /home/paradox/JetsonYolov5/yoloDet.py THEN import pycuda.autoinit
+import pycuda.driver as cuda
 cuda.init()
+device = cuda.Device(0)
+ctx = device.make_context()
+host_ip = ''  # Accept connections on any interface
+port = 5800
+backlog = 5
+capture_ready=False
 model = YoloTRT(library="yolov5/build/libmyplugins.so", engine="yolov5/build/yolov5s.engine", conf=0.5, yolo_ver="v5")
-# [{'class': 'Note', 'conf': 0.6934612, 'box': array([164.14363, 129.19603, 280.63977, 219.     ], dtype=float32)}]
+class Client:
+    """Lightweight class to store client socket and lock for thread-safety."""
+    def __init__(self, socket, addr):
+        self.socket = socket
+        self.addr = addr
+        self.lock = threading.Lock()  # Lock to ensure thread-safety of send and recv calls
 
-cap = cv2.VideoCapture(0)
-conn=None # while the connection is None, the listening thread doesnt run
-#the listening thread runs when connection is successful 
-port = 5800  # initiate port no above 1024
-host_ip='10.21.2.86' #optimize so we can get IP from hostname
-test_without_connection=True # testing without clients connecting to server
-
-if test_without_connection:
-    print('NOT SENDING DATA, TESTING WITHOUT HOSTS!')
-else:
-    print('PREPARING TO CONNECT WITH HOSTS!')
-def predict():
-    ret, frame = cap.read()
-    if ret:
-        frame = imutils.resize(frame, width=352,height=320)
-        detections, t = model.Inference(frame)
-        #print('DETECTIONS:',detections)
-        # for obj in detections:
-        #    print(obj['class'], obj['conf'], obj['box'])
-        # Only show output if a display is available
-        if 'DISPLAY' in os.environ:
-            cv2.imshow("Output", frame)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                #break
-                pass
-        else:
-            print("No display available, skipping visualization.")
-        boxes=[]
-        if len(detections)>0:
-            for i,v in enumerate(detections):
-                box=v['box']
-                boxes.append(box)
-        
-        return boxes
-    else:
-        print('camera not functioning')
-        return 'camera not functioning'
-
-print(f'hostname:{socket.gethostbyname(socket.gethostname())}')
-server_socket = socket.socket()  # get instance
-#host = socket.gethostname()
-#host_ip = socket.gethostbyname(host)
-if not test_without_connection:
-    server_socket.bind((host_ip, port))  # bind host address and port together
-
-def connect():
-    #server_socket.bind((host_ip, port))  # bind host address and port together
-    #binding is potentially part of connecting?
-    print('listening for connetions...')
-        
-    #listens for up to 5 clients
-    server_socket.listen(5)
-    #makes conn and address accessible to other parts of the code
-    global conn
-    global address
-    connect.conn, connect.address = server_socket.accept()  # accept new connection
-    conn,address=connect.conn, connect.address
-    print("Connection from: " + str(connect.address)) 
-
-def send_data(data,conn,delay=0,verbose=False):
-    conn.sendall(data.encode())  # send data to the client
-    time.sleep(delay)
-    if verbose:
-        print('sent data')
-        print(data)
-def server_program():
-    frame_counter = 1
-    bounding_box_counter=0
-    cuda.init()
-    device = cuda.Device(0)
-    ctx = device.make_context()
-    while True:
-        if not test_without_connection:
-            connect()
-        global conn
-        global address
-        if not test_without_connection:
-            conn= connect.conn
-            address =  connect.address
+class Server:
+    """Server class to accept connections, handle clients, and broadcast data to clients."""
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((host_ip, port))
+        self.server_socket.listen(backlog)
+        self.video_capture = cv2.VideoCapture(0)
+        print(f"[*] Listening on {host_ip}:{port}")
+        self.data_queue = queue.Queue(maxsize=1)  # Only store the latest data
+        self.data_available = threading.Condition()  # Condition variable to signal when new data is available
+        self.clients=[]
+        self.frame_counter=1
+    def start(self):
+        """Start the server and begin accepting connections."""
+        threading.Thread(target=self.accept_connections).start()
+        threading.Thread(target=self.capture_images).start()
+        threading.Thread(target=self.image_processing).start()
+        threading.Thread(target=self.broadcast_data).start()
+    def accept_connections(self):
+        """Accept incoming connections and spawn a new thread for each client."""
         while True:
-            try: 
-                time1=time.time()
-                #after you finished the operations of inference/detection:
-                bounding_boxes=predict()
-                ctx.pop()
+            client_socket, addr = self.server_socket.accept()
+            print(f"[*] Accepted connection from {addr[0]}:{addr[1]}")
+            client = Client(client_socket, addr)
+            self.clients.append(client)
+            threading.Thread(target=self.handle_client, args=(client,)).start()
+            print("CLIENTS:", self.clients)
+    def capture_images(self):
+        while True:
+            global capture_ready
+            global latest_image
+            capture_ready,latest_image = self.video_capture.read()
+            latest_image  = imutils.resize(latest_image, width=640,height=640)
+            self.frame_counter+=1
+    def image_processing(self):
+        """Do image processing and send data."""
+        while True:
+            # TODO: Replace the following line with your image processing code
+            if capture_ready:
                 ctx.push()
-                if bounding_boxes!='camera not functioning':
-                    #preparing the string to be sent over to the robo rio
-                    bounding_box_str=''
-                    #message indicating start
-                    bounding_box_str+= f'''\nF {frame_counter} 352 320\n'''
-    
-                    #bounding box messages
-    
-                    for index,box in enumerate(bounding_boxes):
-                        x1,y1,x2,y2=box
-                        x1,y1,x2,y2 = int(round(x1)),int(round(y1)),int(round(x2)),int(round(y2))
-                        bounding_box_str+=f'R {x1} {y1} {x2} {y2}\n'
-                    
-                    #message indicating end
-                    bounding_box_str+=f'E\n'
-                    print('bounding box str:',bounding_box_str)
-                        
-                    #Sending the data to robo rio
-                    if not test_without_connection:
-                        send_data(bounding_box_str,conn,verbose=True)
-                        
-                    frame_counter+=1
-                    time2=time.time()
-                    time_taken=time2-time1
-                    print('fps:',1/time_taken)
+                time1=time.time()
+                detections, t = model.Inference(latest_image)
+                time2=time.time()
+                time_taken=time2-time1
+                fps=1/time_taken
+                ctx.pop()
+                print('fps:',fps)
+                bounding_boxes=[]
+                if len(detections)>0:
+                    for i,v in enumerate(detections):
+                        box=v['box']
+                        bounding_boxes.append(box)
+                bounding_box_str=''
+                #message indicating start
+                bounding_box_str+= f'''\nF {self.frame_counter} 352 320\n'''
 
-                else:
-                    print('predictions not working because camera is not functioning')
-            except (BrokenPipeError,ConnectionResetError,ConnectionAbortedError,ConnectionError) as e:
-                print('DISCONNECTED. attempting reconnecting...')
-                break  
-    #conn.close() (closes the connection, I'll potentially use this later)
-    ctx.pop()
-def listen():
-    while True:
-        if conn and conn is not None:
-            data = conn.recv(1024).decode()
-            if not data:
-                print('not recieving any data')
-            else:
+                #bounding box messages
+
+                for index,box in enumerate(bounding_boxes):
+                    x1,y1,x2,y2=box
+                    x1,y1,x2,y2 = int(round(x1)),int(round(y1)),int(round(x2)),int(round(y2))
+                    bounding_box_str+=f'R {x1} {y1} {x2} {y2}\n'
+                bounding_box_str+=f'E\n'
+                data=bounding_box_str
                 print('data:',data)
-prediction_thread = threading.Thread(target=server_program) #utilize GPU
-listening_thread = threading.Thread(target=listen)# utilize CPU
+                self.send_data(data)
+            else:
+                print('camera not functioning')
 
-print("TESTING!")
-if __name__ == '__main__':
-    prediction_thread.start()
-    listening_thread.start()
+    def send_data(self, data):
+        """Send data to all connected clients."""
+        if self.data_queue.full():
+            self.data_queue.get()
+        self.data_queue.put(data)
+        with self.data_available:
+            self.data_available.notify_all()  # Signal that new data is available
+
+    def handle_client(self, client):
+        """Handle data received on a client connection."""
+        while True:
+            with client.lock: # Don't allow other threads to send data while we're receiving
+                data = client.socket.recv(1024)
+                if not data:
+                    print('not receiving any data')
+                else:
+                    print('data:',data, client.addr)
+
+    def broadcast_data(self):
+        """Broadcast data to all connected clients."""
+        while True:
+            with self.data_available:
+                self.data_available.wait()  # Wait for new data to be available
+            data = self.data_queue.get()
+            for client in self.clients:
+                print('broadcasting...', client.addr)
+                try:
+                    #with client.lock: # Don't allow other threads to receive data while we're sending
+                    client.socket.send(data.encode())
+                except Exception as e:
+                    print("Removing client", client.addr, e)
+                    self.clients.remove(client) 
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
